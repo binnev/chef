cc126c657ee2f1e9e9a09fd76d119361f68060cc
binnev <robin.m.neville@gmail.com>
2024-03-19 22:02:42 +0100

    test: added test for init existing library

--- tests/test_api/test_initialise.py
+++ tests/test_api/test_initialise.py
@@ -7,7 +7,9 @@ from src.api.constants import __app_name__
 
 @patch("src.api.initialise.open")
 @patch("src.api.initialise.Path.mkdir")
+@patch("src.api.initialise.Path.exists", return_value=False)
 def test_init_library__new(
+    mock_exists: MagicMock,
     mock_mkdir: MagicMock,
     mock_open: MagicMock,
 ):
@@ -21,3 +23,19 @@ def test_init_library__new(
         path / f".{__app_name__}",
     ]
     mock_open.assert_called_with(path / ".yes-chef/settings.json", "w")
+    mock_exists.assert_called()
+
+
+@patch("src.api.initialise.open")
+@patch("src.api.initialise.Path.mkdir")
+@patch("src.api.initialise.Path.exists", return_value=True)
+def test_init_library__existing(
+    mock_exists: MagicMock,
+    mock_mkdir: MagicMock,
+    mock_open: MagicMock,
+):
+    path = Path("foo/bar")
+    init_library(path)
+    assert mock_mkdir.call_count == 4
+    mock_open.assert_not_called()
+    mock_exists.assert_called()
