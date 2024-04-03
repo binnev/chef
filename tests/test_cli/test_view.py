05606642b1d1bd075a334cd8a69c48d85781672c
binnev <robin.m.neville@gmail.com>
2024-04-03 20:03:32 +0200

    test: fix test with no access to filesystem in CI

--- tests/test_cli/test_view.py
+++ tests/test_cli/test_view.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 import pytest
 from typer.testing import CliRunner
 
+from src.api import Plan
 from src.api.shopping_list import MergedIngredient
 from src.cli import app
 from src.cli.view import _format_ingredient_for_list
@@ -74,11 +75,17 @@ def test__format_ingredient_for_list(
 
 
 @patch("src.api.plan.Plan.shopping_list")
-def test_view_list__empty(mock_shopping_list, recipe_library_initialised):
+@patch("src.cli.view.api.Plan.current")
+def test_view_list__empty(
+    mock_current,
+    mock_shopping_list,
+    recipe_library_initialised,
+):
     """
     This tests a bug where the formatting (which used `max`) would break for
     an empty plan.
     """
+    mock_current.return_value = Plan()
     mock_shopping_list.return_value = []
     runner = CliRunner()
     result = runner.invoke(app, "view list".split())
