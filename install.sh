dc8b8544465987196af9e71659c78c6bb766e210
binnev <robin.m.neville@gmail.com>
2024-04-27 11:52:36 +0200

    build: install script now uses pipx

--- install.sh
+++ install.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-
-name="chef"
-
-# build
-pyinstaller entrypoint.py --name $name --onefile --clean
-
-# install
-cp dist/$name ~/.local/bin
-
-#clean up
-rm -R build
-rm -R dist
-rm $name.spec
+pipx install . --force
